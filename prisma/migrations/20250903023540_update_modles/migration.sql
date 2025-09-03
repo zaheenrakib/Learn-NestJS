00fd4f071cfd56362b04173b70ee9579e1dbbcd7
zaheenrakib <rakibzaheen@gmail.com>
2025-09-03 08:39:38 +0600

    01:11:10 sec . unique email error

---
+++ prisma/migrations/20250903023540_update_modles/migration.sql
@@ -0,0 +1,44 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Bookmark` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "public"."Bookmark";
+
+-- DropTable
+DROP TABLE "public"."User";
+
+-- CreateTable
+CREATE TABLE "public"."users" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."bookmarks" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "bookmarks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."bookmarks" ADD CONSTRAINT "bookmarks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,13 +9,24 @@ export class AuthService {
 
     }
 
-    signup(dto: AuthDto) {
+    async signup(dto: AuthDto) {
         // generate the password hash
-
+        const hash = await argon.hash(dto.password)
         // save the new user in the db
-
+        const user  = await this.prisma.user.create({
+            data:{
+                email:dto.email,
+                hash,
+            },
+            // select:{
+            //     id:true,
+            //     email: true,
+            //     createdAt: true,
+            // }
+        });
+        // delete user.hash;
         // return the saved user
-        return {msg: "I have signed up"}
+        return user
 
     }
 
