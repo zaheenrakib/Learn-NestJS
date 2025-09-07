b7449b7334e377d77711b29ddac41a3e4adcb9ee
zaheenrakib <rakibzaheen@gmail.com>
2025-09-07 08:37:12 +0600

    1.30.00 sec

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,13 +5,15 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient{
-    constructor(){
+    constructor(prisma: PrismaService){
         super({
             datasources:{
                 db:{
                     url: 'postgresql://postgres:123@localhost:5434/nest'
                 }
             }
-        })
+        });
+        prisma.user.findMany();
     }
+
 }
